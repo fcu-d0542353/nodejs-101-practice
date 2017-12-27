582a0bf428be8c1619cf1000497ff0ba35db6848
fcu-d0542353 <32894223+fcu-d0542353@users.noreply.github.com>
2017-12-27 16:35:37 +0800

    Create practice1.js

---
+++ day1/D0542353/practice1.js
@@ -0,0 +1,21 @@
+{
+  "name": "hsunchi",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+	"homework":"echo \"hello world\"",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "axios": "^0.17.1",
+    "eslint": "^4.13.1",
+    "jquery": "^3.2.1",
+    "nodemon": "^1.13.3"
+  },
+  "devDependencies": {
+    "mocha": "^4.0.1"
+  }
+}
